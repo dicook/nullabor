ec2aa40a2d4826e1f79b4bdce683c0926e337562
Dianne Cook <visnut@gmail.com>
2014-10-26 16:33:39 -0500

    cleaned up function documentation

--- R/distmet.r
+++ R/distmet.r
@@ -2,7 +2,8 @@
 #'
 #' The empirical distribution of the distance measures is calculated based on the mean
 #' distance of each of the null plots from the other null plots in a lineup. At this moment
-#' this method works only for \code{\link{null_permute}} method.
+#' this method works only for \code{\link{null_permute}} method. This function helps get some
+#' assessment of whether the actual data plot is very different from the null plots.
 #'
 #' @export
 #' @param lineup.dat lineup data
@@ -14,6 +15,13 @@
 #' default
 #' @param dist.arg a list or vector of inputs for the distance metric met; NULL by default
 #' @param m the number of plots in the lineup; m = 20 by default
+#' @return lineup has the data used for the caulations
+#' @return null_values contains new null samples from which to compare nulls in lineup
+#' @return diff difference in distance between nulls and actual data and that of the null
+#' that is most different from other nulls. A negative value means that the actual data
+#' plot is similar to the null plots.
+#' @return closest list of the five closest nulls to the actual data plot
+#' @return pos position of the actual data plot in the lineup
 #' @importFrom dplyr summarise group_by
 #' @examples
 #' if(require('dplyr')){
@@ -36,8 +44,7 @@
 #' distmet(lineup.dat, var = c('mpg', 'wt'), 'bin_dist', null_permute('mpg'), pos = 4, repl =
 #' 100, dist.arg = list(lineup.dat = lineup.dat, X.bin = 5, Y.bin = 5), m = 8)}
 #'
-#' #Example using uni_dist
-#'
+#' # Example using uni_dist
 #' \donttest{
 #' mod <- lm(wt ~ mpg, data = mtcars)
 #' resid.dat <- data.frame(residual = mod$resid)
@@ -47,7 +54,7 @@
 #' #decrypt('....') #Copy and paste to get the true position
 #' if(require('dplyr')){
 #' distmet(lineup.dat, var = 'residual', 'uni_dist', null_dist('residual', dist =
-#' 'normal'), pos = 19, repl = 100)} 
+#' 'normal'), pos = 19, repl = 100)}
 #' # Assuming pos = 19; but put the true position for pos
 #' }
 distmet <- function(lineup.dat, var, met, method, pos, repl = 1000, dist.arg = NULL, m = 20) {
@@ -59,45 +66,46 @@ distmet <- function(lineup.dat, var, met, method, pos, repl = 1000, dist.arg = N
     	null <- method(obs.dat)
     	null_gen(lineup.dat, null, met, method, m, dist.arg)
     	})
-   return(list(lineup = dist.mean[, c(pos.1 = "plotno", dist = "mean.dist")], null_values = all.samp, diff = diff, 
+   return(list(lineup = dist.mean[, c(pos.1 = "plotno", dist = "mean.dist")], null_values = all.samp, diff = diff,
         closest = closest, pos = pos))
 }
 
 #' Computing th distance for the null plots
-#' 
+#'
 #' @keywords internal
 null_gen <- function(lineup.dat, null, met, method, m, dist.arg){
 	func <- match.fun(met)
 	Dist <- replicate(m - 2, {
 		null.dat <- method(null)
-		 ifelse(is.null(dist.arg), do.call(func, list(null, null.dat)), 
+		 ifelse(is.null(dist.arg), do.call(func, list(null, null.dat)),
 		do.call(func, append(list(null, null.dat), unname(dist.arg))))
-	})	
+	})
 	mean(Dist)
 }
 
-
 #' Plotting the distribution of the distance measure
 #'
-#' The distribution of the distance measure is plotted with the distances for 
-#' the null plots and true plot overlaid.  
+#' The permutation distribution of the distance measure is plotted with the distances for
+#' the null plots. Distance measure values for the null plots and the true plot are overlaid.
 #'
 #' @param dat output from \code{\link{distmet}}
 #' @param m the number of plots in the lineup; m = 20 by default
 #' @export
-#' @examples 
+#' @examples
 #' if(require('dplyr')){
-#' distplot(distmet(lineup(null_permute('mpg'), mtcars, pos = 1), var = c('mpg',
-#' 'wt'), 'reg_dist', null_permute('mpg'), pos = 1, repl = 100, m = 8), m = 8) }
+#' d <- lineup(null_permute('mpg'), mtcars, pos = 1)
+#' qplot(mpg, wt, data=d) + facet_wrap(~.sample)
+#' distplot(distmet(d, var = c('mpg', 'wt'), 'reg_dist', null_permute('mpg'),
+#' pos = 1, repl = 100, m = 8), m = 8) }
 distplot <- function(dat, m = 20) {
-    p <- with(dat, qplot(null_values, geom = "density", fill = I("grey80"), colour = I("grey80"), 
-        xlab = "Permutation distribution", ylab = "") + geom_segment(aes(x = lineup$mean.dist[lineup$plotno != 
-        pos], xend = lineup$mean.dist[lineup$plotno != pos], y = rep(0.01 * min(density(null_values)$y), 
-        (m - 1)), yend = rep(0.05 * max(density(null_values)$y), (m - 1))), size = 1, alpha = I(0.7)) + 
-        geom_segment(aes(x = lineup$mean.dist[lineup$plotno == pos], xend = lineup$mean.dist[lineup$plotno == 
-            pos], y = 0.01 * min(density(null_values)$y), yend = 0.1 * max(density(null_values)$y)), 
-            colour = "darkorange", size = 1) + geom_text(data = lineup, y = -0.03 * max(density(null_values)$y), 
-        size = 2.5, aes(x = mean.dist, label = plotno)) + ylim(c(-0.04 * max(density(null_values)$y), 
+    p <- with(dat, qplot(null_values, geom = "density", fill = I("grey80"), colour = I("grey80"),
+        xlab = "Permutation distribution", ylab = "") + geom_segment(aes(x = lineup$mean.dist[lineup$plotno !=
+        pos], xend = lineup$mean.dist[lineup$plotno != pos], y = rep(0.01 * min(density(null_values)$y),
+        (m - 1)), yend = rep(0.05 * max(density(null_values)$y), (m - 1))), size = 1, alpha = I(0.7)) +
+        geom_segment(aes(x = lineup$mean.dist[lineup$plotno == pos], xend = lineup$mean.dist[lineup$plotno ==
+            pos], y = 0.01 * min(density(null_values)$y), yend = 0.1 * max(density(null_values)$y)),
+            colour = "darkorange", size = 1) + geom_text(data = lineup, y = -0.03 * max(density(null_values)$y),
+        size = 2.5, aes(x = mean.dist, label = plotno)) + ylim(c(-0.04 * max(density(null_values)$y),
         max(density(null_values)$y) + 0.1)))
     return(p)
-} 
+}
