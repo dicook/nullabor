9f46993273ae5dcf19a20668c7297a507a6d6f62
Dianne Cook <visnut@gmail.com>
2016-05-03 14:23:00 +1000

    added data documentation

---
+++ R/data.r
@@ -0,0 +1,24 @@
+#' Los Angeles Lakers play-by-play data.
+#'
+#' Play by play data from all games played by the Los Angeles lakers in the
+#' 2008/2009 season.
+#'
+#' @docType data
+#' @name lal
+NULL
+
+#' Wasp gene expression data.
+#'
+#' Data from Toth et al (2010) used in Niladri Roy et al (2015)
+#'
+#' @docType data
+#' @name wasps
+NULL
+
+#' Sample turk results
+#'
+#' Subset of data from a Turk experiment, used to show how to compute power of a lineup
+#'
+#' @docType data
+#' @name turk_results
+NULL
--- R/lal.r
+++
@@ -1,8 +0,0 @@
-#' Los Angeles Lakers play-by-play data.
-#'
-#' Play by play data from all games played by the Los Angeles lakers in the
-#' 2008/2009 season.
-#'
-#' @docType data
-#' @name lal
-NULL 
